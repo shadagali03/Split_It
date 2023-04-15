6a513100601d1af583caa067b2c226f4fdb63edf
Dev <devextraemail@gmail.com>
2023-04-15 15:26:10 -0400

    initialized all pages and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home.js';
+import { Home, Dashboard, Groups, Addexpense } from './pages';
 import 'tailwindcss/tailwind.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+          <BrowserRouter>
+            <Routes>
+              <Route path ="/" element={<Home />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/groups" element={<Groups />} />
+              <Route path="/addexpense" element={<Addexpense />} />
+            </Routes>
+          </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Addexpense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Addexpense = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+        Addexpense
+    </div>
+  );
+}
+
+export default Addexpense;

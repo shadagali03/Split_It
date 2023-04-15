942d1448a9ea85f90cdc7f0cad712a9f2aea048b
Dev <devextraemail@gmail.com>
2023-04-15 15:31:45 -0400

    test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
           <BrowserRouter>
+          
             <Routes>
               <Route path ="/" element={<Home />} />
               <Route path="/dashboard" element={<Dashboard />} />
